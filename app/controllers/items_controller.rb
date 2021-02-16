bc02cc513859442adb44d7adecafef19075e95a8
HiraiD <uk060528@yahoo.ne.jp>
2021-02-16 18:28:03 +0900

    商品詳細表示機能　１回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user! , except: :index
+  before_action :authenticate_user! , except: [:index, :show]
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -8,7 +8,18 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  # def edit
+  #   @item = Item.find(params[:id])
+  # end
  
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  # def destroy
+  #   @item = Item.destroy
+  # end
+
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -24,5 +35,4 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name,:informaion,:category_id,:burden_of_shipping_charges_id,:shipping_area_id,:estimated_shipping_id,:image,:product_condition_id,:price).merge(user_id: current_user.id)
   end
-
 end
