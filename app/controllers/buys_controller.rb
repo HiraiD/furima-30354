07e99bd32d24560b037c53192d04857b1b220d26
HiraiD <uk060528@yahoo.ne.jp>
2021-03-01 17:23:43 +0900

    訂正　２

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,20 +1,20 @@
 class BuysController < ApplicationController
-  before_action :authenticate_user!, except: [:create]
-    def index
-      @item = Item.find(params[:item_id])
+  before_action :authenticate_user!, only: [:create,:index]
+  before_action :con_item, only: [:index, :create]
+  before_action :app_item, only: [:index,:creates,:purchase_params,:app_item]
+ 
+  def index
+      @user_purchase = Purchase.new
       if current_user.id == @item.user_id
        #ログインしているユーザー　＝＝　商品を出品した人(何を記述しているのか記載)
-
-    @user_purchase = Purchase.new
     redirect_to root_path
-  else
+    else
     render action: :index
     end
   end
 
   
-def creates
-    @item = Item.find(params[:item_id])
+  def create
     pay_item
     @user_purchase = Purchase.new(purchase_params)
       # binding.pry
@@ -35,22 +35,23 @@ def creates
       user_id: current_user.id, item_id: params[:item_id], token: params[:token]
     )
   end
+  
+  def pay_item
+    Payjp.api_key = 'sk_test_0a13d9a08559b03b92a6abc7' # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price, # 商品の値段
+      card: purchase_params[:token], # カードトークン
+      currency: 'jpy' # 通貨の種類（日本円）
+      )
+  end
+    
+  def app_item
+    redirect_to root_path if @item.buy != nil && current_user
+    #しょうひんが売れていたらという条件式を書く。show.html.erbのSoldOutで使われているぶぶんを参考にする。
+  end
+  
+  #＠itemを定義するメソッドを作成
+    def con_item
+      @item = Item.find(params[:item_id])
+    end
 end
-
-def pay_item
-  Payjp.api_key = 'sk_test_0a13d9a08559b03b92a6abc7' # 自身のPAY.JPテスト秘密鍵を記述しましょう
-  Payjp::Charge.create(
-    amount: @item.price, # 商品の値段
-    card: purchase_params[:token], # カードトークン
-    currency: 'jpy' # 通貨の種類（日本円）
-  )
-end
-
-# def set_item
-# @item = Item.find(params[:item_id])
-# end
-
-# ①ストロングパラメータでフォームからの情報を受け取る。
-# ②ストロングパラメータで受け取った情報を元に、form_objectのインスタンスを作る。
-# ③作ったインスタンスがバリデーションを通過できるか検証をする。
-# ④作ったインスタンスをsaveする。（フォームオブジェクト内に定義したsaveメソッドを使用）
